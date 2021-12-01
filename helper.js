396d17966e9c34c368f3b2950e49df28691b3a5e
james.rollings <james.rollings@civica.co.uk>
2021-12-01 19:47:24 +0000

    add toArray option

--- helper.js
+++ helper.js
@@ -1,10 +1,11 @@
 import { resolve } from 'path';
 import { promises as fs } from 'fs';
 
-export default (async function getInputData(day){
+export default (async function getInputData(day, toArray = true){
     try {
         const filePath = resolve(process.cwd(), day, 'data.js');
-        return await fs.readFile(filePath, { encoding: 'utf-8' });
+        const data = await fs.readFile(filePath, { encoding: 'utf-8' })
+        return toArray ? data.split('\n').map((row) => row.trim()) : data;
     } catch (err) {
         console.error(err);
         process.exit(1);
