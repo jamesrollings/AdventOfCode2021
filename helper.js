d1e23d7bf6268975ec6456fd192379ff09d4fa15
james.rollings <james.rollings@civica.co.uk>
2021-12-02 09:14:11 +0000

    changed to using txt file for data

--- helper.js
+++ helper.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 
 export default async function getInputData(day, toArray = true){
     try {
-        const filePath = resolve(process.cwd(), day, 'data.js');
+        const filePath = resolve(process.cwd(), day, 'data.txt');
         const data = await fs.readFile(filePath, { encoding: 'utf-8' })
         return toArray ? data.split('\n').map((row) => row.trim()) : data;
     } catch (err) {
