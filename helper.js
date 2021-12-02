e2058c66a1f3b85bbf00dbfd028218d7d12c8aeb
james.rollings <james.rollings@civica.co.uk>
2021-12-02 11:57:39 +0000

    fix issues with helper file not always picking up the correct path

--- helper.js
+++ helper.js
@@ -1,9 +1,10 @@
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
 import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
 
 export default async function getInputData(day, toArray = true){
     try {
-        const filePath = resolve(process.cwd(), day, 'data.txt');
+        const filePath = resolve(fileURLToPath(dirname(import.meta.url)), day, 'data.txt');
         const data = await fs.readFile(filePath, { encoding: 'utf-8' })
         return toArray ? data.split('\n').map((row) => row.trim()) : data;
     } catch (err) {
