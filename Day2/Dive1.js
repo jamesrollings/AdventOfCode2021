aa4a0d3edb02345a3ab47480e98a2eecd04af6e7
james.rollings <james.rollings@civica.co.uk>
2021-12-02 09:28:29 +0000

    Day2 part 1

---
+++ Day2/Dive1.js
@@ -0,0 +1,30 @@
+import getInputData from '../helper.js';
+
+function instructionCreator(strInstruction) {
+    const [position, unit] = strInstruction.split(' ');
+    return [position, Number(unit)];
+}
+
+export default (async () => {
+    const data = await getInputData('Day2');
+
+    const instructions = data.map((instruction) => instructionCreator(instruction));
+
+    let horizontal = 0;
+    let depth = 0;
+
+    for (let i=0; i < instructions.length; i++) {
+        const arrInstruction = instructions[i];
+        if (arrInstruction[0] === 'forward') {
+            horizontal += arrInstruction[1];
+        }
+        if (arrInstruction[0] === 'down') {
+            depth += arrInstruction[1];
+        }
+        if (arrInstruction[0] === 'up') {
+            depth -= arrInstruction[1];
+        }
+    }
+
+    return console.log(horizontal * depth);
+})();
